a7b7ad37e69f7d53aa11767f3ad1779cfa34f5d4
Vladimir <122758630+N-Vladimire@users.noreply.github.com>
2026-03-30 18:14:04 +0200

    Add /env route to return environment variable

--- app/app.py
+++ app/app.py
@@ -20,3 +20,6 @@ def info():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+@app.route("/env")
+def env():
+    return {"env": os.getenv("ENV")}
